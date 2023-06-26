1dd8a1713afbf540dbd40ba94f6a544574863c88
Donghyeon Lee <earthicko@gmail.com>
2023-06-26 10:04:12 +0000

    MVP

--- Makefile
+++ Makefile
@@ -1,17 +1,35 @@
+VOLUME_PATH	:= ${HOME}/data
+HOST_LINK	:= "127.0.0.1	donghyle.42.fr" > /etc/hosts
+
 all:
-	docker compose -f ./srcs/docker-compose.yml up -d --build
+	make prune
+	make build
+
+prune:
+	chmod 666 /var/run/docker.sock
+	mkdir -p $(VOLUME_PATH)
+	mkdir -p $(VOLUME_PATH)/mariadb/
+	mkdir -p $(VOLUME_PATH)/wordpress/
+	echo $(HOST_LINK)
+
+build:
+	docker compose -f ./srcs/docker-compose.yml up --build -d
+
+up:
+	docker compose -f ./srcs/docker-compose.yml up -d
 
 down:
 	docker compose -f ./srcs/docker-compose.yml down
 
-re:
-	docker compose -f ./srcs/docker-compose.yml up -d --build
-
 clean:
-	docker stop $$(docker ps -qa);\
-	docker rm $$(docker ps -qa);\
-	docker rmi -f $$(docker images -qa);\
-	docker volume rm $$(docker volume ls -q);\
-	docker network rm $$(docker network ls -q);\
+	docker compose -f ./srcs/docker-compose.yml down --rmi all --volumes
 
-.PHONY: all re down clean
+fclean:
+	make clean
+	rm -rf $(VOLUME_PATH)
+
+re:
+	make fclean
+	make all
+
+.PHONY: all down clean fclean re
---
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+# 파일의 소유자를 변경하는 명령
+chown -R www-data:www-data /var/www/
+	# -R: Change the user ID and/or the group ID 
+	# for the file hierarchies rooted in the files instead of just the files themselves.
+
+if [ ! -f "/var/www/html/wordpress/index.php" ]; then
+	sudo -u www-data sh -c " \
+	wp core download --locale=$WORDPRESS_LANG && \
+	wp config create --dbname=$WORDPRESS_DB_HOST --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8"
+	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PASSWORD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL --skip-email && \
+	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PASSWORD && \
+	wp plugin update --all
+	"
+fi
+
+# wp core download: Downloads and extracts WordPress core files to the specified path
+	# --locale=<locale>: Select which language you want to download
+# wp config create: Creates a new wp-config.php with database constants, and verifies that the database constants are correct.
+	# --dbname=<dbname>: Set the database name.
+	# --dbuser=<dbuser>: Set the database user.
+	# --dbpass=<dbpass>: Set the database user password.
+	# --dbhost=<dbhost>: Set the database host. (default: localhost)
+	# --dbcharset=<dbcharset>: Set the database charset. (default: utf8)
+# wp core install: Creates the WordPress tables in the database using the URL, title, and default admin user details provided
+	# --url=<url>: The address of the new site.
+	# --title=<site-title>: The title of the new site.
+	# --admin_user=<username>: The name of the admin user.
+	# --admin_password=<password>: The password for the admin user. Defaults to randomly generated string.
+# wp user create: Creates a new user.
+# wp plugin update: Updates one or more plugins.
+
+exec /usr/sbin/php-fpm7.4 -F
